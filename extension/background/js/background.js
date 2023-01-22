2aff1ed312e1060d13f71ee59ca7ab92494674f4
Damian Michael Garcia <damian@outlook.com>
2023-01-21 23:05:01 -0600

    Optimize Storage Handling, Action Updating, Etc.
    (1) Storage handling has been overhauled.
    
    Previously, only the storage.sync API was used along with a
    call-blocking function to limit the frequency of calls to the set
    method. All calls to the set method were blocked and only the last call
    after a gap of 500 milliseconds was used. This implementation has two
    problems. First, 500 milliseconds was short enough that even that could
    trigger the storage.sync quota limit in certain situations. Second, it
    introduced a lag between clicking the "Unhide all jobs" button and
    actually seeing the results in the UI, since the UI changes depend on
    changes caught by a storage.onChanged listener.
    
    These problems have been resolved by using storage.local for everything
    and then allowing the background script to sync storage.local and
    storage.sync with a 2-second call-block.
    
    To prevent storage sync echo, a random string generator and syncId
    function were created and are used to uniquely identify each sync so
    that simultaneous changes to storage.local and storage.sync do not echo.
    
    A function was created to patch occasional data conflicts between
    changes coming from storage.local and storage.sync within the 2-second
    call-block.
    
    (2) The browser action button now has a badge with a tally of what has
    been hidden. The tally increases or decreases by 1 each time a company
    is hidden or unhidden, respectively. It also increases or decreases by 1
    if promoted or sponsored jobs are hidden or unhidden. Additionally, the
    browser action button's title changes to display a short summary of what
    has been hidden.
    
    (3) Despite using message passing between the background page and tabs,
    content-scripts were occasionally injected more than once, causing a
    "doubling" effect of the UI. Instead of message passing to prevent
    multiple script injection, a global variable in the content-script is
    used. Additionally, to prevent multiple CSS injections, the
    content-script now asks the background script to inject the CSS.

--- extension/background/js/background.js
+++ extension/background/js/background.js
@@ -1,64 +1,367 @@
-const supportedJobBoardUrls = [
-  "www.linkedin.com/jobs/search",
-  "www.indeed.com/jobs?",
-];
+class Utilities {
+  static addCallBlockingForRepetitiveCallsButAllowLastCall(
+    functionToCallBlock,
+    minCallFreeTimeRequiredBeforeAllowingNextCall = NaN
+  ) {
+    let callBlockingTimer;
 
-const updateChromeActionForTab = async () => {
-  const [activeTab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
+    const functionWithCallBlockingAdded = (...args) => {
+      clearTimeout(callBlockingTimer);
+      callBlockingTimer = setTimeout(
+        () => functionToCallBlock(...args),
+        minCallFreeTimeRequiredBeforeAllowingNextCall
+      );
+    };
 
-  if (!activeTab) return;
+    return functionWithCallBlockingAdded;
+  }
 
-  const activeTabUrlMatchesSupportedJobBoardUrl = supportedJobBoardUrls.some(
-    (supportedJobUrl) => activeTab.url?.includes(supportedJobUrl)
+  static getRandomNumber({
+    min = NaN,
+    max = NaN,
+    randomIntegersOnly = false,
+  } = {}) {
+    const randomNumber = !randomIntegersOnly
+      ? Math.random() * (max - min) + min
+      : Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
+  }
+
+  static getRandomString(
+    length,
+    { includeUpperCase, includeLowerCase, includeNumber, includeSpecial } = {}
+  ) {
+    const lengthIsNotAnIntegerOrIsLessThanOne =
+      !Number.isInteger(length) || length < 1;
+    if (lengthIsNotAnIntegerOrIsLessThanOne)
+      throw TypeError("length must be an integer greater than zero");
+
+    const allInclusionOptions = [
+      includeUpperCase,
+      includeLowerCase,
+      includeNumber,
+      includeSpecial,
+    ];
+
+    const includeNothing =
+      allInclusionOptions.every((includeOption) => includeOption === false) ||
+      (allInclusionOptions.some(
+        (inclusionOption) => inclusionOption === false
+      ) &&
+        !allInclusionOptions.some(
+          (inclusionOption) => inclusionOption === true
+        ));
+
+    if (includeNothing)
+      throw Error(
+        "At least one type of character must be permitted to generate a string"
+      );
+
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const number = "0123456789";
+    const special = `!"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~`;
+
+    const includeAll = allInclusionOptions.every(
+      (includeOption) => includeOption === undefined
+    );
+
+    const permittedCharacters = includeAll
+      ? upperCase + lowerCase + number + special
+      : `${includeUpperCase ? upperCase : ""}${
+          includeLowerCase ? lowerCase : ""
+        }${includeNumber ? number : ""}${includeSpecial ? special : ""}`;
+
+    return Array.from({ length }, () =>
+      permittedCharacters.charAt(
+        this.getRandomNumber({
+          min: 0,
+          max: permittedCharacters.length - 1,
+          randomIntegersOnly: true,
+        })
+      )
+    ).join("");
+  }
+}
+
+class JobBoards {
+  static #jobBoards = [
+    {
+      jobBoardUrlMatchPattern: "www.linkedin.com/jobs/search",
+      jobBoardId: "linkedIn",
+      jobBoardName: "LinkedIn",
+      jobBoardPromotionalStatusValue: "Promoted",
+    },
+    {
+      jobBoardUrlMatchPattern: "www.indeed.com/jobs?",
+      jobBoardId: "indeed",
+      jobBoardName: "Indeed",
+      jobBoardPromotionalStatusValue: "Sponsored",
+    },
+  ];
+
+  static getJobBoardIdByUrl(url) {
+    return this.#jobBoards.find((jobBoard) =>
+      url?.includes(jobBoard.jobBoardUrlMatchPattern)
+    )?.jobBoardId;
+  }
+
+  static getAllJobBoardIds() {
+    return this.#jobBoards.map((jobBoard) => jobBoard.jobBoardId);
+  }
+
+  static getJobBoardDataByJobBoardId(jobBoardId) {
+    return this.#jobBoards.find(
+      (jobBoard) => jobBoard.jobBoardId === jobBoardId
+    );
+  }
+}
+
+const updateActions = async () => {
+  const activeTabs = await chrome.tabs.query({ active: true });
+
+  if (!activeTabs.length) return;
+
+  const currentWindowId = (await chrome.windows.getCurrent()).id;
+
+  const activeTabInCurentWindow = activeTabs.find(
+    (activeTab) => activeTab.windowId === currentWindowId
   );
 
-  activeTabUrlMatchesSupportedJobBoardUrl
-    ? chrome.action.enable()
-    : chrome.action.disable();
+  if (!activeTabInCurentWindow) return;
+
+  const jobBoardIdOfActiveTabInCurrentWindow = JobBoards.getJobBoardIdByUrl(
+    activeTabInCurentWindow.url
+  );
+
+  if (!jobBoardIdOfActiveTabInCurrentWindow)
+    return chrome.action.disable(activeTabInCurentWindow.id);
+
+  chrome.action.enable(activeTabInCurentWindow.id);
+
+  const activeTabsWithMatchingJobBoardId = activeTabs.filter(
+    (activeTab) =>
+      JobBoards.getJobBoardIdByUrl(activeTab.url) ===
+      jobBoardIdOfActiveTabInCurrentWindow
+  );
+
+  updateBadgeTextAndTitle(
+    activeTabsWithMatchingJobBoardId,
+    jobBoardIdOfActiveTabInCurrentWindow
+  );
 };
+
+const updateBadgeTextAndTitle = async (tabs, jobBoardId) => {
+  const localStorage = await chrome.storage.local.get();
+
+  const { jobBoardName, jobBoardPromotionalStatusValue } =
+    JobBoards.getJobBoardDataByJobBoardId(jobBoardId);
+
+  const blockedCompaniesKey = Object.keys(localStorage).find(
+    (key) =>
+      key.includes(jobBoardId) &&
+      key.includes("companyName") &&
+      key.includes("blockedJobAttributeValues") &&
+      !key.endsWith(".backup")
+  );
+
+  const numberOfBlockedCompaniesForJobBoardId =
+    blockedCompaniesKey && Array.isArray(localStorage[blockedCompaniesKey])
+      ? localStorage[blockedCompaniesKey].length
+      : 0;
+
+  const promotedJobsKey = Object.keys(localStorage).find(
+    (key) =>
+      key.includes(jobBoardId) &&
+      key.includes("promotionalStatus") &&
+      key.includes("blockedJobAttributeValues") &&
+      !key.endsWith(".backup")
+  );
+
+  const promotedJobsAreBlockedForJobBoardId =
+    promotedJobsKey &&
+    Array.isArray(localStorage[promotedJobsKey]) &&
+    localStorage[promotedJobsKey].includes(jobBoardPromotionalStatusValue)
+      ? true
+      : false;
+
+  const numberOfBlockedJobAttributes =
+    numberOfBlockedCompaniesForJobBoardId + promotedJobsAreBlockedForJobBoardId;
+
+  tabs.forEach((tab) => {
+    chrome.action.setTitle({
+      tabId: tab.id,
+      title: `Hide n' Seek
+
+${jobBoardName}:
+• ${numberOfBlockedCompaniesForJobBoardId} ${
+        numberOfBlockedCompaniesForJobBoardId === 1 ? "company" : "companies"
+      } hidden${
+        promotedJobsAreBlockedForJobBoardId
+          ? `\n• All ${jobBoardPromotionalStatusValue.toLowerCase()} jobs hidden`
+          : ""
+      }
+`,
+    });
+
+    chrome.action.setBadgeBackgroundColor({
+      tabId: tab.id,
+      color: [255, 128, 128, 255],
+    });
+
+    chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: `${numberOfBlockedJobAttributes}`,
+    });
+  });
+};
+
+chrome.runtime.onInstalled.addListener(async () => {
+  const syncStorage = await chrome.storage.sync.get();
+  if (Object.keys(syncStorage).length) chrome.storage.local.set(syncStorage);
+});
+
+chrome.storage.local.onChanged.addListener((storageChanges) => {
+  const changesIncludesBlockedJobAttributeValues = Object.keys(
+    storageChanges
+  ).some(
+    (key) =>
+      key.includes("blockedJobAttributeValues") && !key.endsWith(".backup")
+  );
+
+  if (changesIncludesBlockedJobAttributeValues) updateActions();
+});
 
 [
   chrome.runtime.onInstalled,
   chrome.runtime.onStartup,
   chrome.tabs.onActivated,
-].forEach((eventType) =>
-  eventType.addListener(() => updateChromeActionForTab())
-);
+].forEach((eventType) => eventType.addListener(() => updateActions()));
 
 chrome.tabs.onUpdated.addListener(async (tabId, tabChanges, tab) => {
   if (tabChanges.status !== "loading") return;
 
-  updateChromeActionForTab();
+  updateActions();
 
   if (!tab.url) return;
 
-  const tabUrlMatchesSupportedJobBoardUrl = supportedJobBoardUrls.some(
-    (supportedJobUrl) => tab.url.includes(supportedJobUrl)
-  );
+  const jobBoardId = JobBoards.getJobBoardIdByUrl(tab.url);
 
-  if (!tabUrlMatchesSupportedJobBoardUrl) return;
+  if (!jobBoardId) return;
 
-  const inject = () => {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    files: ["/content-script/js/content-script.js"],
+  });
+});
+
+chrome.runtime.onMessage.addListener(async (message, sender) => {
+  if (message.text === "inject css") {
     chrome.scripting.insertCSS({
-      target: { tabId },
+      target: { tabId: sender.tab.id },
       files: ["/content-script/css/content-script.css"],
     });
-    chrome.scripting.executeScript({
-      target: { tabId },
-      files: ["/content-script/js/content-script.js"],
-    });
-  };
-
-  try {
-    const contentScriptResponse = await chrome.tabs.sendMessage(tabId, {
-      message: "ping",
-    });
-
-    if (contentScriptResponse.message !== "ping") inject();
-  } catch {
-    inject();
+  } else if (message.text === "update badge") {
+    const { jobBoardId } = message;
+    const tabs = await chrome.tabs.query({});
+    const tabsWithJobBoardId = tabs.filter(
+      (tab) => JobBoards.getJobBoardIdByUrl(tab.url) === jobBoardId
+    );
+    updateBadgeTextAndTitle(tabsWithJobBoardId, jobBoardId);
   }
+});
+
+const getSyncConflictPatch = (storageChanges) => {
+  const jobBoardIdSyncConflictData = JobBoards.getAllJobBoardIds().map(
+    (jobBoardId) => ({
+      blockedJobAttributeValuesKeys: Object.keys(storageChanges).filter(
+        (key) =>
+          key.includes(jobBoardId) &&
+          key.includes("blockedJobAttributeValues") &&
+          !key.includes("backup")
+      ),
+
+      backupAvailable: Object.entries(storageChanges).some(
+        ([key, value]) =>
+          key.includes(jobBoardId) &&
+          key.includes("blockedJobAttributeValues") &&
+          key.includes("backup") &&
+          value?.length
+      ),
+    })
+  );
+
+  return Object.fromEntries(
+    jobBoardIdSyncConflictData
+      .filter(({ backupAvailable }) => backupAvailable)
+      .flatMap(({ blockedJobAttributeValuesKeys }) =>
+        blockedJobAttributeValuesKeys.map((key) => [key, []])
+      )
+  );
+};
+
+let syncId = "";
+const generateSyncId = () => `${Utilities.getRandomString(8)}.${Date.now()}`;
+
+const storageChangesPendingSet = {};
+const exportStorageChangesToSyncStorage =
+  Utilities.addCallBlockingForRepetitiveCallsButAllowLastCall(async () => {
+    const localStorage = await chrome.storage.local.get();
+
+    syncId = generateSyncId();
+
+    const mergedStorageChanges = Object.assign(
+      {},
+      localStorage,
+      storageChangesPendingSet,
+      { syncId }
+    );
+
+    const syncConflictPatch = getSyncConflictPatch(mergedStorageChanges);
+
+    const patchedStorageChanges = Object.assign(
+      mergedStorageChanges,
+      syncConflictPatch
+    );
+
+    chrome.storage.sync.set(patchedStorageChanges);
+    chrome.storage.local.set(patchedStorageChanges);
+  }, 2000);
+
+chrome.storage.onChanged.addListener((storageChanges, storageArea) => {
+  const syncIdExists = Object.hasOwn(storageChanges, "syncId");
+
+  const syncIdIsTheOnlyChange =
+    syncIdExists && Object.keys(storageChanges).length === 1;
+
+  const syncIdIsOld =
+    syncIdExists &&
+    Object.hasOwn(storageChanges.syncId, "newValue") &&
+    storageChanges.syncId.newValue === syncId;
+
+  if (syncIdIsTheOnlyChange || syncIdIsOld) return;
+
+  const syncStorageWasClearedOnAnotherDevice =
+    storageArea === "sync" &&
+    Object.values(storageChanges).every(
+      (value) =>
+        Object.hasOwn(value, "oldValue") && !Object.hasOwn(value, "newValue")
+    );
+
+  const updateStorageChangesPendingSet = (valueAge) =>
+    Object.assign(
+      storageChangesPendingSet,
+      Object.fromEntries(
+        Object.entries(storageChanges).map(([key, value]) => [
+          key,
+          value[valueAge],
+        ])
+      )
+    );
+
+  !syncStorageWasClearedOnAnotherDevice
+    ? updateStorageChangesPendingSet("newValue")
+    : updateStorageChangesPendingSet("oldValue");
+
+  exportStorageChangesToSyncStorage();
 });
