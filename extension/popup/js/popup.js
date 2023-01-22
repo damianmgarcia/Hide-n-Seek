1789ba4fb09a7d4b653c654fba549cbd68a1b619
Damian Michael Garcia <damian@outlook.com>
2023-01-21 23:11:16 -0600

    RemoveHiddenJobsManager, Optimize Storage Handling
    (1) RemoveHiddenJobsManager was added to handle user requests to remove
    hidden jobs from display.
    
    (2) All storage.sync getters and setters were changed to storage.local
    getters and setters. Storage syncing is now handled by the background
    script. Function that were previously call-blocked have been
    un-call-blocked since storage.local does not have quotas for
    write operations per unit time.
    
    (3) Calls to storage setters have been optimized by setting properties
    in bulk instead of key by key.

--- extension/popup/js/popup.js
+++ extension/popup/js/popup.js
@@ -1,34 +1,15 @@
 (async () => {
-  class Utilities {
-    static addCallBlockingForRepetitiveCallsButAllowLastCall(
-      functionToCallBlock,
-      minCallFreeTimeRequiredBeforeAllowingNextCall = NaN
-    ) {
-      let callBlockingTimer;
-
-      const functionWithCallBlockingAdded = (...args) => {
-        clearTimeout(callBlockingTimer);
-        callBlockingTimer = setTimeout(
-          () => functionToCallBlock(...args),
-          minCallFreeTimeRequiredBeforeAllowingNextCall
-        );
-      };
-
-      return functionWithCallBlockingAdded;
-    }
-  }
-
   class JobBoard {
     static #jobBoards = [
       {
-        name: "linkedIn",
         hostname: "www.linkedin.com",
+        jobBoardId: "linkedIn",
         logoSrc: "/images/linkedin-logo.svg",
         logoAlt: "The logo for LinkedIn.com",
       },
       {
-        name: "indeed",
         hostname: "www.indeed.com",
+        jobBoardId: "indeed",
         logoSrc: "/images/indeed-logo.svg",
         logoAlt: "The logo for Indeed.com",
       },
@@ -39,12 +20,12 @@
   }
 
   class UnblockAllJobsManager {
-    #jobBoard;
+    #jobBoardId;
     #logoSrc;
     #logoAlt;
 
-    constructor(jobBoard, logoSrc, logoAlt) {
-      this.#jobBoard = jobBoard;
+    constructor(jobBoardId, logoSrc, logoAlt) {
+      this.#jobBoardId = jobBoardId;
       this.#logoSrc = logoSrc;
       this.#logoAlt = logoAlt;
     }
@@ -53,100 +34,114 @@
     #optionButton = document.querySelector(".option-button");
     #undoButton = document.querySelector(".undo-button");
 
-    start() {
-      this.#updateElementsBasedOnStorage();
-      chrome.storage.onChanged.addListener((changes) =>
-        this.#updateElementsBasedOnStorage(
-          Object.fromEntries(
-            Object.entries(changes).map(([key, value]) => [key, value.newValue])
-          )
-        )
-      );
+    start(initialStorage) {
+      this.#updateElementsBasedOnStorage(initialStorage);
+      chrome.storage.local.onChanged.addListener((storageChanges) => {
+        const syncIdIsTheOnlyChange =
+          Object.hasOwn(storageChanges, "syncId") &&
+          Object.keys(storageChanges).length === 1;
+        if (syncIdIsTheOnlyChange) return;
+        this.#updateElementsBasedOnStorage();
+      });
       this.#jobBoardName.setAttribute("src", this.#logoSrc);
       this.#jobBoardName.setAttribute("alt", this.#logoAlt);
       this.#optionButton.addEventListener("click", () => this.#unblock());
       this.#undoButton.addEventListener("click", () => this.#undoUnblock());
     }
 
-    #updateElementsBasedOnStorage =
-      Utilities.addCallBlockingForRepetitiveCallsButAllowLastCall(async () => {
-        const storage = await chrome.storage.sync.get();
-        const [
-          blockedJobAttributeValuesFromStorage,
-          blockedJobAttributeValuesBackupFromStorage,
-        ] = await Promise.all([
-          this.#getBlockedJobAttributeValuesFromStorage(storage),
-          this.#getBlockedJobAttributeValuesBackupFromStorage(storage),
-        ]);
+    async #updateElementsBasedOnStorage(initialStorage) {
+      const storage = initialStorage || (await chrome.storage.local.get());
+      const [
+        blockedJobAttributeValuesFromStorage,
+        blockedJobAttributeValuesBackupFromStorage,
+      ] = await Promise.all([
+        this.#getBlockedJobAttributeValuesFromStorage(storage),
+        this.#getBlockedJobAttributeValuesBackupFromStorage(storage),
+      ]);
 
-        const allBlockedJobAttributeValuesFromStorageAreEmpty = Object.values(
-          blockedJobAttributeValuesFromStorage
-        ).every(
+      const allBlockedJobAttributeValuesFromStorageAreEmpty = Object.values(
+        blockedJobAttributeValuesFromStorage
+      ).every((blockedJobAttributeValues) => !blockedJobAttributeValues.length);
+
+      const allBlockedJobAttributeValuesBackupFromStorageAreEmpty =
+        Object.values(blockedJobAttributeValuesBackupFromStorage).every(
           (blockedJobAttributeValues) => !blockedJobAttributeValues.length
         );
 
-        const allBlockedJobAttributeValuesBackupFromStorageAreEmpty =
-          Object.values(blockedJobAttributeValuesBackupFromStorage).every(
-            (blockedJobAttributeValues) => !blockedJobAttributeValues.length
-          );
-
-        if (
-          !allBlockedJobAttributeValuesFromStorageAreEmpty &&
-          allBlockedJobAttributeValuesBackupFromStorageAreEmpty
-        ) {
-          this.#optionButton.disabled = false;
-          this.#undoButton.disabled = true;
-        } else if (
-          allBlockedJobAttributeValuesFromStorageAreEmpty &&
-          !allBlockedJobAttributeValuesBackupFromStorageAreEmpty
-        ) {
-          this.#optionButton.disabled = true;
-          this.#undoButton.disabled = false;
-        } else if (
-          allBlockedJobAttributeValuesFromStorageAreEmpty &&
-          allBlockedJobAttributeValuesBackupFromStorageAreEmpty
-        ) {
-          this.#optionButton.disabled = true;
-          this.#undoButton.disabled = true;
-        }
-      }, 33);
+      if (
+        !allBlockedJobAttributeValuesFromStorageAreEmpty &&
+        allBlockedJobAttributeValuesBackupFromStorageAreEmpty
+      ) {
+        this.#optionButton.disabled = false;
+        this.#undoButton.disabled = true;
+      } else if (
+        allBlockedJobAttributeValuesFromStorageAreEmpty &&
+        !allBlockedJobAttributeValuesBackupFromStorageAreEmpty
+      ) {
+        this.#optionButton.disabled = true;
+        this.#undoButton.disabled = false;
+      } else if (
+        allBlockedJobAttributeValuesFromStorageAreEmpty &&
+        allBlockedJobAttributeValuesBackupFromStorageAreEmpty
+      ) {
+        this.#optionButton.disabled = true;
+        this.#undoButton.disabled = true;
+      }
+    }
 
     async #unblock() {
+      this.#optionButton.disabled = true;
+
       const blockedJobAttributeValuesFromStorage =
         await this.#getBlockedJobAttributeValuesFromStorage();
 
-      await Promise.all(
-        Object.keys(blockedJobAttributeValuesFromStorage).map((key) =>
-          chrome.storage.sync.remove(key)
-        )
-      );
+      const entries = Object.entries(blockedJobAttributeValuesFromStorage);
 
-      await this.#backupBlockedJobAttributeValues(
-        blockedJobAttributeValuesFromStorage
-      );
+      if (!entries.length) return;
+
+      const storageChangesToSet = Object.fromEntries([
+        ...entries.map(([key]) => [key, []]),
+        ...entries.map(([key, value]) => [`${key}.backup`, value]),
+      ]);
+
+      await chrome.storage.local.set(storageChangesToSet);
+
+      chrome.runtime.sendMessage({
+        text: "update badge",
+        jobBoardId: this.#jobBoardId,
+      });
     }
 
     async #undoUnblock() {
+      this.#undoButton.disabled = true;
+
       const blockedJobAttributeValuesBackupFromStorage =
         await this.#getBlockedJobAttributeValuesBackupFromStorage();
 
-      await Promise.all(
-        Object.entries(blockedJobAttributeValuesBackupFromStorage).map(
-          ([key, value]) =>
-            chrome.storage.sync.set({ [key.replace(".backup", "")]: value })
-        )
-      );
-
-      await this.#clearBlockedJobAttributeValuesBackup(
+      const entries = Object.entries(
         blockedJobAttributeValuesBackupFromStorage
       );
+
+      if (!entries.length) return;
+
+      const storageChangesToSet = Object.fromEntries([
+        ...entries.map(([key]) => [key, []]),
+        ...entries.map(([key, value]) => [key.replace(".backup", ""), value]),
+      ]);
+
+      await chrome.storage.local.set(storageChangesToSet);
+
+      chrome.runtime.sendMessage({
+        text: "update badge",
+        jobBoardId: this.#jobBoardId,
+      });
     }
 
     async #getBlockedJobAttributeValuesFromStorage(storage) {
       return Object.fromEntries(
-        Object.entries(storage || (await chrome.storage.sync.get())).filter(
+        Object.entries(storage || (await chrome.storage.local.get())).filter(
           ([key]) =>
-            key.includes(this.#jobBoard) &&
+            key.includes(this.#jobBoardId) &&
             key.includes("blockedJobAttributeValues") &&
             !key.endsWith(".backup")
         )
@@ -155,54 +150,89 @@
 
     async #getBlockedJobAttributeValuesBackupFromStorage(storage) {
       return Object.fromEntries(
-        Object.entries(storage || (await chrome.storage.sync.get())).filter(
+        Object.entries(storage || (await chrome.storage.local.get())).filter(
           ([key]) =>
-            key.includes(this.#jobBoard) &&
+            key.includes(this.#jobBoardId) &&
             key.includes("blockedJobAttributeValues.backup")
         )
       );
     }
+  }
 
-    async #backupBlockedJobAttributeValues(
-      blockedJobAttributeValuesFromStorage
-    ) {
-      await Promise.all(
-        Object.entries(
-          blockedJobAttributeValuesFromStorage ||
-            (await this.#getBlockedJobAttributeValuesFromStorage())
-        ).map(([key, value]) =>
-          chrome.storage.sync.set({ [`${key}.backup`]: value })
-        )
-      );
+  class RemoveHiddenJobsManager {
+    #removeHiddenJobsStorageKey;
+
+    constructor(jobBoardId) {
+      this.#removeHiddenJobsStorageKey = `JobDisplayManager.${jobBoardId}.removeHiddenJobs`;
     }
 
-    async #clearBlockedJobAttributeValuesBackup(
-      blockedJobAttributeValuesBackupFromStorage
-    ) {
-      await Promise.all(
-        Object.keys(
-          blockedJobAttributeValuesBackupFromStorage ||
-            (await this.#getBlockedJobAttributeValuesBackupFromStorage())
-        ).map((key) => chrome.storage.sync.remove(key))
+    #checkboxInput = document.querySelector("input[name='remove-hidden-jobs']");
+    #checkboxLabel = this.#checkboxInput.closest("label");
+
+    async start(initialStorage) {
+      const removeHiddenJobsStorageKeyFound = Object.hasOwn(
+        initialStorage,
+        this.#removeHiddenJobsStorageKey
       );
+
+      const initialRemoveHiddenJobsValue = removeHiddenJobsStorageKeyFound
+        ? initialStorage[this.#removeHiddenJobsStorageKey]
+        : false;
+
+      if (!removeHiddenJobsStorageKeyFound)
+        chrome.storage.local.set({
+          [this.#removeHiddenJobsStorageKey]: false,
+        });
+
+      this.#checkboxInput.checked = initialRemoveHiddenJobsValue;
+      this.#checkboxLabel.dataset.checked = this.#checkboxInput.checked;
+
+      this.#checkboxInput.addEventListener("input", () => {
+        this.#checkboxLabel.dataset.checked = this.#checkboxInput.checked;
+        chrome.storage.local.set({
+          [this.#removeHiddenJobsStorageKey]: this.#checkboxInput.checked,
+        });
+      });
+
+      chrome.storage.local.onChanged.addListener((storageChanges) => {
+        const containsChangesToRemoveHiddenJobs = Object.hasOwn(
+          storageChanges,
+          this.#removeHiddenJobsStorageKey
+        );
+
+        if (!containsChangesToRemoveHiddenJobs) return;
+
+        this.#checkboxInput.checked =
+          storageChanges[this.#removeHiddenJobsStorageKey].newValue;
+        this.#checkboxLabel.dataset.checked = this.#checkboxInput.checked;
+      });
     }
   }
 
-  const [activeTab] = await chrome.tabs.query({
+  const [activeTabInCurrentWindow] = await chrome.tabs.query({
     active: true,
     currentWindow: true,
   });
 
-  if (activeTab) {
-    const hostnameOfTabUrl = activeTab.url?.match(
-      /https:\/\/(?<hostname>[\w.]*)\//
-    )?.groups?.hostname;
-    const jobBoard = JobBoard.getJobBoardByHostname(hostnameOfTabUrl);
-    if (jobBoard)
-      new UnblockAllJobsManager(
-        jobBoard.name,
-        jobBoard.logoSrc,
-        jobBoard.logoAlt
-      ).start();
-  }
+  if (!activeTabInCurrentWindow) return;
+
+  const hostnameOfTabUrl = activeTabInCurrentWindow.url?.match(
+    /https:\/\/(?<hostname>[\w.]*)\//
+  )?.groups?.hostname;
+
+  const jobBoard = JobBoard.getJobBoardByHostname(hostnameOfTabUrl);
+
+  if (!jobBoard) return;
+
+  document.querySelector(".options-container").classList.remove("display-none");
+
+  const initialStorage = await chrome.storage.local.get();
+
+  new UnblockAllJobsManager(
+    jobBoard.jobBoardId,
+    jobBoard.logoSrc,
+    jobBoard.logoAlt
+  ).start(initialStorage);
+
+  new RemoveHiddenJobsManager(jobBoard.jobBoardId).start(initialStorage);
 })();
