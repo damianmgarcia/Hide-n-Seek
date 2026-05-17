8c1d180bbd41e655c9c05ae598fe95d74cf8229c
Damian Michael Garcia <93714639+damianmgarcia@users.noreply.github.com>
2026-05-17 00:43:24 -0500

    (1) Moved initial value getter responsibility to the new initializeStorage function
    (2) Simplify setting names

--- extension/chrome/runtime/popup/settings/display-settings.js
+++ extension/chrome/runtime/popup/settings/display-settings.js
@@ -3,32 +3,16 @@ import { initialStorage } from "../initial-storage.js";
 
 const displaySettings = [
   {
-    storageKey: "setting.global.removeHiddenJobs",
+    storageKey: "removeHiddenJobs",
     checkboxSelector: "[name='remove-hidden-jobs']",
-    getInitialValue() {
-      let initialValue = false;
-      for (const jobBoard of jobBoards) {
-        const legacyKey = `JobDisplayManager.${jobBoard.id}.removeHiddenJobs`;
-        const hasLegacyKey = Object.hasOwn(initialStorage, legacyKey);
-        if (!hasLegacyKey) continue;
-        chrome.storage.local.remove(legacyKey);
-        if (initialValue || !initialStorage[legacyKey]) continue;
-        initialValue = initialStorage[legacyKey];
-      }
-      return initialValue;
-    },
   },
   {
-    storageKey: "setting.global.removeBlockButtons",
+    storageKey: "removeBlockButtons",
     checkboxSelector: "[name='remove-block-buttons']",
-    getInitialValue() {
-      return false;
-    },
   },
 ];
 
-for (const displaySetting of displaySettings) {
-  const { storageKey, checkboxSelector } = displaySetting;
+for (const { storageKey, checkboxSelector } of displaySettings) {
   const checkbox = document.querySelector(checkboxSelector);
   const label = checkbox.closest("label");
 
@@ -52,11 +36,5 @@ for (const displaySetting of displaySettings) {
     updateCheckbox(changes[storageKey].newValue);
   });
 
-  if (Object.hasOwn(initialStorage, storageKey)) {
-    updateCheckbox(initialStorage[storageKey]);
-  } else {
-    chrome.storage.local.set({
-      [storageKey]: displaySetting.getInitialValue(),
-    });
-  }
+  updateCheckbox(initialStorage[storageKey]);
 }
