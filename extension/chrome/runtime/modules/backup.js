a6973731b1b87bd6ac9911d82918d23268b06cc4
Damian Michael Garcia <93714639+damianmgarcia@users.noreply.github.com>
2026-05-17 00:23:38 -0500

    (1) Update restore function so that backup files are processed by the new initializeStorage function

--- extension/chrome/runtime/modules/backup.js
+++ extension/chrome/runtime/modules/backup.js
@@ -1,4 +1,5 @@
 import { download, upload } from "./files.js";
+import { initializeStorage } from "./storage.js";
 
 const backup = async function () {
   const data = await chrome.storage.local.get();
@@ -8,7 +9,7 @@ const backup = async function () {
   const base64Storage = btoa(
     encodedJsonStorage.reduce(function (data, byte) {
       return data + String.fromCodePoint(byte);
-    }, "")
+    }, ""),
   );
   const dataUri = `data:application/json;base64,${base64Storage}`;
   const fileName = `hide-n-seek-backup-${Date.now()}.json`;
@@ -22,8 +23,7 @@ const restore = async function () {
     const file = fileList[0];
     const fileText = await file.text();
     const jsonStorage = JSON.parse(fileText);
-    await chrome.storage.local.clear();
-    await chrome.storage.local.set(jsonStorage);
+    await initializeStorage(jsonStorage);
     return true;
   } catch (error) {
     console.log(error);
