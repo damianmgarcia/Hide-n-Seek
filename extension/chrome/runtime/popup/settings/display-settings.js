b4e4edc3db67dee32218ea04d4dd30bc69f44da3
Damian Michael Garcia <93714639+damianmgarcia@users.noreply.github.com>
2026-07-31 23:19:46 -0500

    (1) Add "Block button action" setting

--- extension/chrome/runtime/popup/settings/display-settings.js
+++ extension/chrome/runtime/popup/settings/display-settings.js
@@ -1,40 +1,71 @@
 import { jobBoards } from "../../modules/job-boards.js";
 import { initialStorage } from "../initial-storage.js";
 
-const displaySettings = [
+const generalSettings = [
   {
     storageKey: "removeHiddenJobs",
-    checkboxSelector: "[name='remove-hidden-jobs']",
+    selector: "[name='remove-hidden-jobs']",
   },
   {
     storageKey: "removeBlockButtons",
-    checkboxSelector: "[name='remove-block-buttons']",
+    selector: "[name='remove-block-buttons']",
+  },
+  {
+    storageKey: "blockButtonAttribute",
+    selector: "[name='block-button-attribute']",
+    options: [
+      { text: "Ask", value: "" },
+      { text: "Block company", value: "company" },
+      { text: "Block job", value: "job" },
+    ],
   },
 ];
 
-for (const { storageKey, checkboxSelector } of displaySettings) {
-  const checkbox = document.querySelector(checkboxSelector);
-  const label = checkbox.closest("label");
+for (const { storageKey, selector, options } of generalSettings) {
+  const setting = document.querySelector(selector);
+  if (setting.matches("input[type='checkbox']")) {
+    const label = setting.closest("label");
 
-  checkbox.addEventListener("input", () => {
-    label.setAttribute("data-checked", checkbox.checked);
-    chrome.storage.local.set({ [storageKey]: checkbox.checked });
-  });
+    setting.addEventListener("input", () => {
+      label.setAttribute("data-checked", setting.checked);
+      chrome.storage.local.set({ [storageKey]: setting.checked });
+    });
 
-  checkbox.addEventListener("keydown", (keyboardEvent) => {
-    if (keyboardEvent.key !== "Enter" || keyboardEvent.repeat) return;
-    checkbox.checked = checkbox.checked ? false : true;
-    checkbox.dispatchEvent(new Event("input"));
-  });
+    setting.addEventListener("keydown", (keyboardEvent) => {
+      if (keyboardEvent.key !== "Enter" || keyboardEvent.repeat) return;
+      setting.checked = setting.checked ? false : true;
+      setting.dispatchEvent(new Event("input"));
+    });
 
-  const updateCheckbox = (value = false) => {
-    checkbox.checked = value;
-    label.setAttribute("data-checked", value);
-  };
-  chrome.storage.local.onChanged.addListener((changes) => {
-    if (!Object.hasOwn(changes, storageKey)) return;
-    updateCheckbox(changes[storageKey].newValue);
-  });
+    const updateSetting = (value = false) => {
+      setting.checked = value;
+      label.setAttribute("data-checked", value);
+    };
+    chrome.storage.local.onChanged.addListener((changes) => {
+      if (!Object.hasOwn(changes, storageKey)) return;
+      updateSetting(changes[storageKey].newValue);
+    });
 
-  updateCheckbox(initialStorage[storageKey]);
+    updateSetting(initialStorage[storageKey]);
+  } else if (setting.matches("select")) {
+    for (const option of options) {
+      const optionElement = document.createElement("option");
+      optionElement.value = option.value;
+      optionElement.text = option.text;
+      setting.append(optionElement);
+    }
+    setting.addEventListener("input", () => {
+      chrome.storage.local.set({ [storageKey]: setting.value });
+    });
+
+    const updateSetting = (value = setting.options[0]) => {
+      setting.value = value;
+    };
+    chrome.storage.local.onChanged.addListener((changes) => {
+      if (!Object.hasOwn(changes, storageKey)) return;
+      updateSetting(changes[storageKey].newValue);
+    });
+
+    updateSetting(initialStorage[storageKey]);
+  }
 }
